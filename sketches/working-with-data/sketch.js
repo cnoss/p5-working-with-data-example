00807d42b9534d950a8d41c4b199106bc825f514
Christian Noss <christian.noss@th-koeln.de>
2022-11-14 11:10:49 +0100

    Added sketch

---
+++ sketches/working-with-data/sketch.js
@@ -0,0 +1,244 @@
+/*jshint esversion: 6 */
+
+/* ############################################################################ 
+
+Kurs «Generative Gestaltung» an der TH Köln
+Christian Noss
+christian.noss@th-koeln.de
+https://twitter.com/cnoss
+https://cnoss.github.io/generative-gestaltung/
+
+############################################################################ */
+
+const saveParams = {
+  sketchName: "Working with data"
+}
+
+// Params for canvas
+const canvasParams = {
+  holder: document.getElementById('canvas'),
+  state: false,
+  mouseX: false,
+  mouseY: false,
+  mouseLock: false,
+  background: 0,
+  gui: true,
+  mode: 'canvas', // canvas or svg … SVG mode is experimental 
+};
+getCanvasHolderSize();
+
+// Params for the drawing
+const drawingParams = {
+  scale:1,
+  scaleMax: 100,
+  scaleMin: -100
+
+
+};
+
+// Params for logging
+const loggingParams = {
+  targetDrawingParams: document.getElementById('drawingParams'),
+  targetCanvasParams: document.getElementById('canvasParams'),
+  state: false
+};
+
+
+let rawData;
+let data = {};
+
+
+/* ###########################################################################
+Classes
+############################################################################ */
+
+
+
+
+/* ###########################################################################
+Custom Functions
+############################################################################ */
+
+function prepareData(){
+  console.log(rawData);
+  data.items = rawData.features;
+}
+
+/* ###########################################################################
+P5 Functions
+############################################################################ */
+
+
+function preload() {
+  const url = "./data/events-in-koeln.json"
+  rawData = loadJSON(url);
+}
+
+function setup() {
+
+  let canvas;
+  if (canvasParams.mode === 'SVG') {
+    canvas = createCanvas(canvasParams.w, canvasParams.h, SVG);
+  } else { 
+    canvas = createCanvas(canvasParams.w, canvasParams.h);
+    canvas.parent("canvas");
+  }
+
+  // Display & Render Options
+  angleMode(DEGREES);
+  smooth();
+
+  // GUI Management
+  if (canvasParams.gui) { 
+    const sketchGUI = createGui('Params');
+    sketchGUI.addObject(drawingParams);
+    noLoop();
+  }
+
+  // Anything else
+  colorMode(HSB, 360, 100, 100, 100);
+  fill(200);
+  noStroke();
+  background(0);
+
+  prepareData();
+  
+}
+
+
+
+function draw() {
+
+  /* ----------------------------------------------------------------------- */
+  // Log globals
+  if (!canvasParams.mouseLock) {
+    canvasParams.mouseX = mouseX;
+    canvasParams.mouseY = mouseY;
+    logInfo();
+  }
+
+  /* ----------------------------------------------------------------------- */
+  // Provide your Code below
+  background(0,0,0, 90);
+  const scale = drawingParams.scale;
+
+  for(let i = 0; i < data.items.length; i++){
+    const item = data.items[i];
+    const {attributes} = item;
+    const from = attributes.datum_von;
+    const to = attributes.datum_bis;
+    const duration = (to - from) / 1000 / 60 / 60 / 24;
+
+    const x = map(item.geometry.x, 6.5, 7.5, scale * 10, width -scale * 10 );
+    const y = map(item.geometry.y, 50.5 , 51.5, scale * 10, height -scale * 10);
+    const r = map(duration, 0, 5000, 0, 50);
+    fill(0, 0, 100, 50);
+    ellipse(x, y, r, r);
+  }
+
+}
+
+
+
+function keyPressed() {
+
+  if (keyCode === 81) { // Q-Key
+  }
+
+  if (keyCode === 87) { // W-Key
+  }
+
+  if (keyCode === 89) { // Y-Key
+  }
+
+  if (keyCode === 88) { // X-Key
+  }
+
+  if (keyCode === 83) { // S-Key
+    const suffix = (canvasParams.mode === "canvas") ? '.jpg' : '.svg';
+    const fragments = location.href.split(/\//).reverse();
+    const suggestion = fragments[1] ? fragments[1] : 'gg-sketch';
+    const fn = prompt(`Filename for ${suffix}`, suggestion);
+    save(fn + suffix);
+  }
+
+  if (keyCode === 49) { // 1-Key
+  }
+
+  if (keyCode === 50) { // 2-Key
+  }
+
+  if (keyCode === 76) { // L-Key
+    if (!canvasParams.mouseLock) {
+      canvasParams.mouseLock = true;
+    } else { 
+      canvasParams.mouseLock = false;
+    }
+    document.getElementById("canvas").classList.toggle("mouseLockActive");
+  }
+
+
+}
+
+
+
+function mousePressed() {}
+
+
+
+function mouseReleased() {
+  
+}
+
+
+
+function mouseDragged() {}
+
+
+
+function keyReleased() {
+  if (keyCode == DELETE || keyCode == BACKSPACE) clear();
+}
+
+
+
+
+
+/* ###########################################################################
+Service Functions
+############################################################################ */
+
+
+
+function getCanvasHolderSize() {
+  canvasParams.w = canvasParams.holder.clientWidth;
+  canvasParams.h = canvasParams.holder.clientHeight;
+}
+
+
+
+function resizeMyCanvas() {
+  getCanvasHolderSize();
+  resizeCanvas(canvasParams.w, canvasParams.h);
+}
+
+
+
+function windowResized() {
+  resizeMyCanvas();
+}
+
+
+
+function logInfo(content) {
+
+  if (loggingParams.targetDrawingParams) {
+    loggingParams.targetDrawingParams.innerHTML = helperPrettifyLogs(drawingParams);
+  }
+
+  if (loggingParams.targetCanvasParams) {
+    loggingParams.targetCanvasParams.innerHTML = helperPrettifyLogs(canvasParams);
+  }
+
+}
+
